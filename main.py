009579db783d737b8280d90c02eae1830ca61ddc
ismlrn <ismail.rahmanikhah@gmail.com>
2025-05-21 05:42:25 +0330

    Changing Convolutional layer to work with multiple filters

--- main.py
+++ main.py
@@ -14,25 +14,38 @@ print("Input shape:", input_image.shape)
 # adding convolution layer with no padding and stride = 1
 def conv2d(X, kernel, stride=1):
     B, C, H, W = X.shape
-    _, _, KH, KW = kernel.shape
+    F, _, KH, KW = kernel.shape
     out_h = (H - KH) // stride + 1
     out_w = (W - KW) // stride + 1
 
-    out = np.zeros((B, 1, out_h, out_w))
+    out = np.zeros((B, F, out_h, out_w))
 
     for b in range(B):
-        for i in range(0, out_h):
-            for j in range(0, out_w):
-                region = X[b, 0, i:i+KH, j:j+KW]
-                out[b, 0, i, j] = np.sum(region * kernel[0, 0])
+        for f in range(F):
+            for i in range(out_h):
+                for j in range(out_w):
+                    region = X[b, 0, i:i+KH, j:j+KW]
+                    out[b, f, i, j] = np.sum(region * kernel[f, 0])
     return out
+
 
 
 # %%
 # creating a 3*3 filter with random values
-kernel = np.array([[[[1, 0, -1],
-                     [1, 0, -1],
-                     [1, 0, -1]]]])  # shape (1, 1, 3, 3)
+# kernel = np.array([[[[1, 0, -1],
+#                      [1, 0, -1],
+#                      [1, 0, -1]]]])  # shape (1, 1, 3, 3)
+
+kernel = np.array([
+    [[[1, 0, -1],
+      [1, 0, -1],
+      [1, 0, -1]]],  # filter 1 (edge detection)
+
+    [[[0, 1, 0],
+      [0, 1, 0],
+      [0, 1, 0]]]   # filter 2 (vertical focus)
+])  # shape (2, 1, 3, 3)
+
 
 conv_out = conv2d(input_image, kernel)
 # the output shape is (1, 1, 6, 6)
